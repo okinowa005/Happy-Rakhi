a0ed8f3f14d2c0029248cafda2feabb466fe3b8a
okinowa005 <vinitiny13@gmail.com>
2024-08-15 16:15:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.getElementById('main-image').addEventListener('click', function() {
     this.src = 'rakhi.jpg'; // Update this with the actual rakhi image path
+    document.getElementById('second-text').classList.remove('hidden');
     setTimeout(function() {
         document.getElementById('main-image').classList.add('hidden');
+        document.getElementById('second-text').classList.add('hidden');
         document.getElementById('message').classList.remove('hidden');
-    }, 10000); // 5 seconds delay
+    }, 5000); // 5 seconds delay
 });
